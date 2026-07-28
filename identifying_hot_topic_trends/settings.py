7b8d90d369eb755db9cd5953b73dd8dc47e7e784
SYED DANISH ALI <danishsyed237@gmail.com>
2026-07-28 21:49:47 +0530

    Update HTML templates and settings

--- identifying_hot_topic_trends/settings.py
+++ identifying_hot_topic_trends/settings.py
@@ -116,7 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'Template/images')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Template/static'),
+    os.path.join(BASE_DIR, 'Template/images'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Template/media')
 
